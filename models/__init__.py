18c620875cf42b4988237a7fa682a1217e2d9c67
Nyaadu <nyaadu@gmail.com>
2024-11-17 16:44:06 +0000

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-'''__init__ file'''
+"""Module for FileStorage autoinit."""
 from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
-
-
 storage = FileStorage()
 storage.reload()
