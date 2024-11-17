39eca7fddc78fa84ac20634ea927e4e778a084fb
Nyaadu <nyaadu@gmail.com>
2024-11-17 15:31:33 +0000

    Update console.py

--- console.py
+++ console.py
@@ -240,12 +240,14 @@ class HBNBCommand(cmd.Cmd):
     def help_update(self):
         '''HELP_UPDATE'''
         print(
-                "Update Command t Updates an instance based on the <Model_name>\
+                "Command Updates an instance based on the <Model_name>\
                 \nand <id> by adding or updating attribute.\
                 \nExample:\
                 \n> update <Model_name> <id> <attribute name> <value>\
                 \n"
         )
 
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
+    
